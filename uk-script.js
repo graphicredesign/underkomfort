c6858b2e4d75a723929c4e1e6885918485673fc9
graphicredesign <30274348+graphicredesign@users.noreply.github.com>
2021-05-14 20:39:02 -0700

    Organized code
    Got code together for the scroll animation

--- uk-script.js
+++ uk-script.js
@@ -1,40 +1,78 @@
 // JavaScript Document
 
+
+//Global
+var pagetl = gsap.timeline({paused:true});
+ pagetl.set(".box-animation", {
+     autoAlpha: 0,
+     display: "none"
+ });
+ pagetl.to(".main-other", 1, {
+     autoAlpha: 1
+ });
+ pagetl.set([".down-arrow-button"], {
+     autoAlpha: 0,
+     display: "none"
+ });
+
+var scrollArrowTl = gsap.timeline({paused:true});
+scrollArrowTl.from(".scroll-arrow-cont", 1, {
+    y: "50"
+});
+scrollArrowTl.to(".scroll-arrow-cont", 1, {
+    opacity: 1
+});
+
 var video = document.querySelector("#v0");
 
-/*
-function detectMob() {
- return ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) );
-}
-  
-window.onresize = function() {
-	
-	console.log(detectMob())
-	
-	if(detectMob() == true){
-
-		console.log("mobile detected");
-		console.log("hide video");
-		//document.querySelector(video).style.display = "block"; //make video visible
-
-	}else{
-
-		console.log("desktop detected");
-		console.log("show video");
-		//document.querySelector(video).style.display = "none"; //make video visible
-
-	}
-	
-}
-*/
-
-var detector = new MobileDetect(window.navigator.userAgent)
+/** Detect Mobile **/
+var detector = new MobileDetect(window.navigator.userAgent);
 
 if(detector.mobile() == null ){
 	
 	document.querySelector('.section.main-section').style.height = 10000 + "px";
 	video.play();
 	TweenMax.delayedCall(3.5, video.pause, null, video);
+	//TweenMax.delayedCall(3.6, startScrollBasedAnimation);
+	video.onpause = function() {
+    	startScrollBasedAnimation();
+		console.log('paused video')
+	};
+	
+	//** Setup scroll for video **//
+	const intro = document.querySelector("#main-section");
+    var frameNumber = 0,
+        playbackConst = 1000,
+        vid = document.getElementById('v0');    
+	
+	function startScrollBasedAnimation(){
+	
+		function scrollPlay() {
+
+			//detect if first page load
+			var frameNumber;
+			frameNumber = window.pageYOffset / playbackConst;
+			
+			//track CURRENT SCROLL POSITION
+			vid.currentTime = frameNumber;
+
+			//fade out VIDEO FRAME
+			if (frameNumber > 9.8) {
+				pagetl.play();
+				console.log("after fold, fade in web page");
+			} else {
+				pagetl.reverse();
+				console.log("before fold, fade out web page");
+			}
+
+			window.requestAnimationFrame(scrollPlay);
+		}
+		
+		window.requestAnimationFrame(scrollPlay);
+		
+	}
+	
+    
 	
 	console.group(`DESKTOP`);
 	console.log(`Is video present? ${video}`);
@@ -46,21 +84,11 @@ if(detector.mobile() == null ){
 	video.stop(); //stop video in case it plays in non-IOS Safari browsers/devices
 	video.style.display = "none"; //remove video
 	
-	console.group(`MOBILE`);
-	console.log(`Is video present? ${video}`);
-	console.groupEnd();
 	
+	console.group(`MOBILE`);
+    console.log(`Is video present? ${video}`);
+    console.groupEnd();
 }
-
-
-/*if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
- 	console.log("mobile");
-	document.querySelector(".body-id").style.background = "red";
-}else{
-	console.log("desktop");
-	document.querySelector(".body-id").style.background = "red";
-}*/
-
 
 /*function detectMob() {
     return ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) );
